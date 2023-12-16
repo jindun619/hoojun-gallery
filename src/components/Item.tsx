93153ec0676b5433024f17fcf03156b0f49850e3
jindun619 <jindun619@naver.com>
2023-12-16 05:33:05 +0000

    1

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -6,9 +6,12 @@ interface ItemProps {
 
 export function Item({ src, title, desc }: ItemProps) {
   return (
-    <div className="py-10 text-center border-t-2 border-green-200">
+    <div className="py-3 sm:py-10 text-center border-t-2 border-green-200">
       <div className="p-1 bg-emerald-100">
-        <img className="w-full h-[450px] object-contain" src={src} />
+        <img
+          className="w-full h-[250px] sm:h-[450px] object-contain"
+          src={src}
+        />
       </div>
       <h3 className="mt-3 text-3xl font-semibold">{title}</h3>
       <p className="mt-2 text-xl text-slate-600">{desc}</p>
