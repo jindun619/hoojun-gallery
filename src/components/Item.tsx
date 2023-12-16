0eb72b6591b77cc731bc5513ec956859cd61c1d3
jindun619 <jindun619@naver.com>
2023-12-16 05:51:33 +0000

    using next/image

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 interface ItemProps {
   src: string;
   title: string;
@@ -8,10 +10,9 @@ export function Item({ src, title, desc }: ItemProps) {
   return (
     <div className="py-3 sm:py-10 text-center border-t-2 border-green-200">
       <div className="p-1 bg-emerald-100">
-        <img
-          className="w-full h-[250px] sm:h-[450px] object-contain"
-          src={src}
-        />
+        <div className="relative w-full h-[250px] sm:h-[450px]">
+          <Image src={src} alt={src} fill={true} className="object-contain" />
+        </div>
       </div>
       <h3 className="mt-3 text-3xl font-semibold">{title}</h3>
       <p className="mt-2 text-xl text-slate-600">{desc}</p>
